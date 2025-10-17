6cff468a2ab9be0bad4afd536dd01b910c5bbc1d
Jhonatan Betancur <jhon.alex.01082000@gmail.com>
2025-10-16 21:03:53 -0500

    configure microservice with NATS

--- users-ms/src/main.ts
+++ users-ms/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { envs } from './config/enviroments';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('main')
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: ['nats://localhost:4222'],
+      
+    }
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,7 +21,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(envs.PORT);
-  console.log(`✅ App running in port: ${envs.PORT}`);
+  await app.listen();
+  logger.log(`App running in port: ${envs.PORT}`)
 }
 bootstrap();
